fa779720998e2196ad8b7ee9522c7cb5f94ba605
Miiduoa <107848266+Miiduoa@users.noreply.github.com>
2025-03-20 10:21:05 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,36 +1,32 @@
-from flask import Flask, render_template, request
-from datetime import datetime,timezone,timedelta
+from waitress import serve
 
+if __name__ == "__main__":
+    # app.run()
+    serve(app, host='0.0.0.0', port=8080)
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>顧晉瑋Python網頁</h1>"
-    homepage += "<a href='/today'>顯示日期時間</a><br>"
-    homepage += "<a href='/about'>顧晉瑋簡介網頁</a><br>"
-    homepage += "<a href='/account'>網頁表單傳值</a><br>"
-    return homepage
+    return "Hello World!"
 
 @app.route("/today")
 def today():
-    # 設定時區為 UTC+8（臺灣）
-    tz = timezone(timedelta(hours=+8))
-    now = datetime.now(tz)
+    now = datetime.now()
     return render_template("today.html", datetime=str(now))
 
-@app.route("/about")
-def about():
-    return "<h1>顧晉瑋簡介網頁</h1><p>這裡放個人簡介內容，例如個人網頁、聯絡資訊等。</p>"
-
-@app.route("/account", methods=["GET", "POST"])
+if __name__ == "__main__":
+    app.run()
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+    @app.route("/account", methods=["GET", "POST"])
 def account():
     if request.method == "POST":
         user = request.form["user"]
         pwd = request.form["pwd"]
-        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
         return result
     else:
         return render_template("account.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
